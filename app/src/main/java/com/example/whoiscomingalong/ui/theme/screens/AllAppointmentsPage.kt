401b57123cb50850d696f481fb538910b9433459
CarinaBaeck <cabaeck@edu.aau.at>
2024-07-15 20:28:29 +0200

    added design to all screens

--- app/src/main/java/com/example/whoiscomingalong/ui/theme/screens/AllAppointmentsPage.kt
+++ app/src/main/java/com/example/whoiscomingalong/ui/theme/screens/AllAppointmentsPage.kt
@@ -1,36 +1,59 @@
 package com.example.whoiscomingalong.ui.theme.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.whoiscomingalong.AppBarWithBackButton
+import com.example.whoiscomingalong.R
 import com.example.whoiscomingalong.WhoIsComingAlongTheme
 
 @Composable
 fun AllAppointmentsPage(navController: NavController) {
-    Scaffold(
-        topBar = { AppBarWithBackButton(navController, "All Appointments") }
-    ) { innerPadding ->
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White)
+            .verticalScroll(rememberScrollState()) // Enable scrolling
+    ) {
         Column(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding)
-                .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+                .fillMaxWidth()
+                .padding(horizontal = 30.dp, vertical = 20.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                text = "All Appointments Page",
-                fontSize = 30.sp,
-                style = MaterialTheme.typography.bodyLarge
+            Spacer(modifier = Modifier.height(40.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.logo_header_transparent),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(72.dp)
             )
+
+            Spacer(modifier = Modifier.height(0.dp))
+
+            HorizontalDivider(
+                modifier = Modifier.width(40.dp),
+                thickness = 1.dp,
+                color = Color.Black
+            )
+
+            Spacer(modifier = Modifier.height(5.dp))
         }
     }
 }
