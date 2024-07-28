f6068024f003da982fa2b09e3378e9f07a7ce8ca
Baciu Alexandra <60070747+baciiu@users.noreply.github.com>
2024-07-28 18:06:01 +0200

    [ISSUE #1] update Retrofit Client

--- app/src/main/java/com/example/whoiscomingalong/Network/RetrofitClient.kt
+++ app/src/main/java/com/example/whoiscomingalong/Network/RetrofitClient.kt
@@ -1,19 +1,42 @@
 package com.example.whoiscomingalong.Network
 
+import com.google.gson.GsonBuilder
+import okhttp3.Credentials
+import okhttp3.Interceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 
-class RetrofitClient {
-    private var retrofit: Retrofit? = null
+object RetrofitClient {
+    private const val BASE_URL = "http://10.0.2.2:8080/"
 
-    fun getClient(baseUrl: String): Retrofit {
+    private val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
 
-        if (retrofit == null) {
-            retrofit = Retrofit.Builder()
-                .baseUrl(baseUrl)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-        }
-        return retrofit!!
+    private val authInterceptor = Interceptor { chain ->
+        val original = chain.request()
+        val requestBuilder = original.newBuilder()
+            .header("Authorization", Credentials.basic("user", "password"))
+        val request = requestBuilder.build()
+        chain.proceed(request)
+    }
+
+    private val httpClient = OkHttpClient.Builder().apply {
+        addInterceptor(logging)
+        addInterceptor(authInterceptor)
+    }.build()
+
+    private val gson = GsonBuilder()
+        .setLenient()
+        .create()
+
+    val instance: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(httpClient)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
     }
 }
