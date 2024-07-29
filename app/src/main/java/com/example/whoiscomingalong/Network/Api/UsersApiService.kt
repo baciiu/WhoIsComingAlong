cc31372d89d7dcb4f414e722409a9efd44e3485a
Baciu Alexandra <60070747+baciiu@users.noreply.github.com>
2024-07-29 22:39:19 +0200

    [ISSUE #1] update UsersApiService

--- app/src/main/java/com/example/whoiscomingalong/Network/Api/UsersApiService.kt
+++ app/src/main/java/com/example/whoiscomingalong/Network/Api/UsersApiService.kt
@@ -1,13 +1,23 @@
 package com.example.whoiscomingalong.Network.Api
 
+import com.example.whoiscomingalong.database.Users.Users
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
-import retrofit2.http.PUT
 import retrofit2.http.Path
 interface UsersApiService {
+    @GET("/users")
+    fun getAllUsers(): Call<List<Users>>
 
+    @GET("/users/{id}")
+    fun getUserById(@Path("id") id: Int): Call<Users>
+
+    @POST("/users")
+    fun createUser(@Body user: Users): Call<Users>
+
+    @DELETE("/users/{id}")
+    fun deleteUser(@Path("id") id: Int): Call<Void>
 
 }
