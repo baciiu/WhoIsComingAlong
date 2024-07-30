ce2efbacb3bd02e4c555a8eb3d001db29c1127b9
Baciu Alexandra <60070747+baciiu@users.noreply.github.com>
2024-07-31 00:45:08 +0200

    [ISSUE #1] replace mocks with repository

--- app/src/main/java/com/example/whoiscomingalong/ui/theme/screens/appointmentscreen/AppointmentScreenViewModel.kt
+++ app/src/main/java/com/example/whoiscomingalong/ui/theme/screens/appointmentscreen/AppointmentScreenViewModel.kt
@@ -3,10 +3,10 @@ package com.example.whoiscomingalong.ui.theme.screens.appointmentscreen
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.whoiscomingalong.dependencyinjection.MockRepository
-import com.example.whoiscomingalong.mocks.appointment.MockAppointment
-import com.example.whoiscomingalong.mocks.appointment.MockAppointmentRepository
-import com.example.whoiscomingalong.mocks.appointment.UserToAppointment
+import com.example.whoiscomingalong.database.Appointment.Appointment
+import com.example.whoiscomingalong.database.Appointment.AppointmentRepository
+import com.example.whoiscomingalong.database.UserToAppointment.UserToAppointment
+import com.example.whoiscomingalong.database.UserToAppointment.UserToAppointmentRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -17,20 +17,21 @@ import javax.inject.Inject
 @HiltViewModel
 class AppointmentScreenViewModel @Inject constructor(
     application: Application,
-    @MockRepository private val mockAppointmentRepository: MockAppointmentRepository
+    private val appointmentRepository: AppointmentRepository,
+    private val userToAppointmentRepository: UserToAppointmentRepository,
 ) : AndroidViewModel(application) {
 
     // StateFlow to hold the list of UserToAppointment
     private val _appointments = MutableStateFlow<List<UserToAppointment>>(emptyList())
     val appointments: StateFlow<List<UserToAppointment>> = _appointments
 
-    private val _appointmentDetails = MutableStateFlow<MockAppointment?>(null)
-    val appointmentDetails: StateFlow<MockAppointment?> = _appointmentDetails
+    private val _appointmentDetails = MutableStateFlow<Appointment?>(null)
+    val appointmentDetails: StateFlow<Appointment?> = _appointmentDetails
 
     init {
         // Initialize by fetching all UserToAppointments
         viewModelScope.launch {
-            mockAppointmentRepository.getAllUserToAppointments().collect { userToAppointments ->
+            userToAppointmentRepository.getAllUserToAppointments().collect { userToAppointments ->
                 _appointments.value = userToAppointments
             }
         }
@@ -39,9 +40,9 @@ class AppointmentScreenViewModel @Inject constructor(
     // Function to update a UserToAppointment
     fun updateUserToAppointment(userToAppointment: UserToAppointment) {
         viewModelScope.launch {
-            mockAppointmentRepository.updateUserToAppointment(userToAppointment)
+            userToAppointmentRepository.updateUserToAppointment(userToAppointment)
             // Optionally re-fetch data after update to ensure the UI is up-to-date
-            mockAppointmentRepository.getAllUserToAppointments().collect { userToAppointments ->
+            userToAppointmentRepository.getAllUserToAppointments().collect { userToAppointments ->
                 _appointments.value = userToAppointments
             }
         }
@@ -49,13 +50,13 @@ class AppointmentScreenViewModel @Inject constructor(
 
     // Function to get all UserToAppointments
     fun getAllUserToAppointments(): Flow<List<UserToAppointment>> {
-        return mockAppointmentRepository.getAllUserToAppointments()
+        return userToAppointmentRepository.getAllUserToAppointments()
     }
 
     // Function to get a specific appointment by its ID
     fun getAppointmentById(appointmentId: Int) {
         viewModelScope.launch {
-            mockAppointmentRepository.getAppointmentById(appointmentId).collect { appointment ->
+            appointmentRepository.getAppointmentById(appointmentId).collect { appointment ->
                 _appointmentDetails.value = appointment
             }
         }
