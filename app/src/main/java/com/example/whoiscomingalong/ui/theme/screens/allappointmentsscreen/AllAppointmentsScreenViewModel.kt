dbad46c8caf294b552a26d8a7d1ae81997a79e97
Angie Drucks <angie.drucks@gmail.com>
2024-07-31 18:44:51 +0200

    Updated AllAppointmentsScreen and AllAppointmentsScreenViewModel to remove mock data and integrate with Room database again.

--- app/src/main/java/com/example/whoiscomingalong/ui/theme/screens/allappointmentsscreen/AllAppointmentsScreenViewModel.kt
+++ app/src/main/java/com/example/whoiscomingalong/ui/theme/screens/allappointmentsscreen/AllAppointmentsScreenViewModel.kt
@@ -3,10 +3,12 @@ package com.example.whoiscomingalong.ui.theme.screens.allappointmentsscreen
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.whoiscomingalong.dependencyinjection.MockRepository
-import com.example.whoiscomingalong.mocks.appointment.MockAppointment
-import com.example.whoiscomingalong.mocks.appointment.MockAppointmentRepository
+import com.example.whoiscomingalong.database.Appointment.AppointmentRepository
+import com.example.whoiscomingalong.database.Appointment.Appointment
+import com.example.whoiscomingalong.database.Users.Users
+import com.example.whoiscomingalong.database.Users.UsersRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -15,17 +17,29 @@ import javax.inject.Inject
 @HiltViewModel
 class AllAppointmentsScreenViewModel @Inject constructor(
     application: Application,
-    @MockRepository private val appointmentRepository: MockAppointmentRepository
+    private val appointmentRepository: AppointmentRepository,
+    private val usersRepository: UsersRepository
 ) : AndroidViewModel(application) {
 
-    private val _appointments = MutableStateFlow<List<MockAppointment>>(emptyList())
-    val appointments: StateFlow<List<MockAppointment>> = _appointments
+    private val _appointments = MutableStateFlow<List<Appointment>>(emptyList())
+    val appointments: StateFlow<List<Appointment>> = _appointments
 
     init {
         viewModelScope.launch {
-            appointmentRepository.getAllAppointmentsForUser(1).collect { appointments ->
+            appointmentRepository.getAllAppointments().collect { appointments ->
                 _appointments.value = appointments
             }
         }
     }
+
+    fun getUserById(userId: Int): StateFlow<Users?> {
+        val userFlow = MutableStateFlow<Users?>(null)
+        viewModelScope.launch {
+            usersRepository.getUserById(userId).collect { user ->
+                userFlow.value = user
+            }
+        }
+        return userFlow
+    }
+
 }
